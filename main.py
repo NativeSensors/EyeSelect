2b9a720241885e4b77c1ac7719117ab5db8afd89
PeterWaIIace <walas.piotr.prywatny@gmail.com>
2025-05-19 18:07:23 +0200

    left right indicators are somehow good

--- main.py
+++ main.py
@@ -15,13 +15,23 @@ def recoverable(func):
 def distance(p1, p2):
     return np.linalg.norm(np.array(p1) - np.array(p2))
 
+def compute_std(buffer):
+    if len(buffer) < 2:
+        return (0.0, 0.0)  # Not enough data
+    arr = np.array(buffer)
+    std_x = np.std(arr[:, 0])
+    std_y = np.std(arr[:, 1])
+    return std_x, std_y
+
+l_buffer = []
+r_buffer = []
 class EyeKeys:
 
     def __init__(self):
         self.finder = FaceFinder()
         self.face = Face()
 
-    @recoverable
+    # @recoverable
     def process(self,image):
 
         face_mesh = self.finder.find(image)
@@ -90,26 +100,56 @@ class EyeKeys:
         ll,lr,lu,ld = l_eye_minMax
         rl,rr,ru,rd = r_eye_minMax
 
-        # todo this does not work
-        width4 = distance(ll,lr)/4
-        height4 = distance(lu,ld)/4
-        print(distance(ll,l_eye_pupil))
-        if distance(ll,l_eye_pupil) < width4:
-            print("look left")
+        lx, ly = l_eye_pupil
+        lx = (lx-lr[0])/(ll[0]-lr[0])
+        ly = (ly-ld[1])/(lu[1]-ld[1])
 
-        if distance(rr,r_eye_pupil) < width4:
-            print("look right")
+        rx, ry = r_eye_pupil
+        rx = (rx-rr[0])/(rl[0]-rr[0])
+        ry = (ry-rd[1])/(ru[1]-rd[1])
 
-        if distance(lu,l_eye_pupil) < height4:
-            print("look up")
+        # Window size
+        width, height = 1920, 1080
 
-        if distance(ld,l_eye_pupil) < height4:
-            print("look down")
+        # Dot position and size
+        l_dot_position = (lx*width, ly*height)  # (x, y)
+        r_dot_position = (rx*width, ry*height)  # (x, y)
+        dot_radius = 5
+        dot_color = (0, 0, 255)  # Red in BGR
+        background_color = (255, 255, 255)  # White
+
+        # Create a white background image
+        image = np.full((height, width, 3), background_color, dtype=np.uint8)
+        # Ensure coordinates are integers
+        if l_dot_position is not None and r_dot_position is not None:
+            l_dot_position = (int(l_dot_position[0]), int(l_dot_position[1]))
+            r_dot_position = (int(r_dot_position[0]), int(r_dot_position[1]))
+            l_buffer.append(l_dot_position)
+            r_buffer.append(r_dot_position)
+
+            for n in range(len(r_buffer)):
+                print(l_dot_position,l_eye_pupil)
+                print(r_dot_position,r_eye_pupil)
+                cv2.circle(image, l_buffer[n], dot_radius, (0, 0, 255), -1)
+                cv2.circle(image, r_buffer[n], dot_radius, (255, 0, 0), -1)
+
+            if len(r_buffer) > 20:
+                r_buffer.pop(0)
+                l_buffer.pop(0)
+
+            l_std_x, l_std_y = compute_std(l_buffer)
+            r_std_x, r_std_y = compute_std(r_buffer)
+
+            print(f"Left eye std: X={l_std_x:.2f}, Y={l_std_y:.2f} x={int(l_dot_position[0]) - width/2} y={int(l_dot_position[1]) - height/2}")
+            print(f"Right eye std: X={r_std_x:.2f}, Y={r_std_y:.2f} x={int(r_dot_position[0]) - width/2} y={int(r_dot_position[1]) - height/2}")
+
+            # Display the image
+            cv2.imshow("Dot Display", image)
+
+            # Wait until a key is pressed
+            cv2.waitKey(1)
 
         # Todo: detect which is closest
-
-
-
 
 if __name__=="__main__":
     ekeys = EyeKeys()
